5c9bfacb72405ecebf6125706d02875724a57c17
Josua Margandatua Hutapea <65341897+hotsince99@users.noreply.github.com>
2021-05-30 16:39:54 +0700

    aaaaa

--- app/src/main/java/com/dicoding/semogabangkit/ui/MainActivity.kt
+++ app/src/main/java/com/dicoding/semogabangkit/ui/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import com.dicoding.semogabangkit.R
 import com.dicoding.semogabangkit.databinding.ActivityMainBinding
+import com.dicoding.semogabangkit.ui.detail_laporan.ReportDetailActivity
 import com.dicoding.semogabangkit.ui.formulir_keluhan.FormActivity
 import com.dicoding.semogabangkit.ui.main_page.MainPageActivity
 
@@ -20,17 +21,22 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         binding.mainPage.setOnClickListener(this)
         binding.formulirKeluhan.setOnClickListener(this)
+        binding.profilPengguna.setOnClickListener(this)
+        binding.detailLaporan.setOnClickListener(this)
+        binding.laporanUntukPemerintah.setOnClickListener(this)
+        binding.registrasi.setOnClickListener(this)
+        binding.login.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.main_page -> {
-                val intent = Intent(this@MainActivity, MainPageActivity::class.java)
+                val intent = Intent(this, MainPageActivity::class.java)
                 startActivity(intent)
             }
 
             R.id.formulir_keluhan -> {
-                val intent = Intent(this@MainActivity, FormActivity::class.java)
+                val intent = Intent(this, FormActivity::class.java)
                 startActivity(intent)
             }
 
@@ -39,7 +45,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             }
 
             R.id.detail_laporan -> {
-
+                val intent = Intent(this, ReportDetailActivity::class.java)
+                intent.putExtra(ReportDetailActivity.EXTRA_ID, 1)
+                startActivity(intent)
             }
 
             R.id.laporan_untuk_pemerintah -> {
